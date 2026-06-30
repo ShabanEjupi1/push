6c0c47f9d207eb4c7bd1ff47455e7d4b3408515d
Shaban Ejupi <shaban.ejupi@m.uni-pr.edu>
2026-06-30 08:53:38 +0000

    push

--- warehouse-1.0-SNAPSHOT/WEB-INF/classes/mk/com/snt/kc/warehouse/persistence/Displayable.java
+++
@@ -1,8 +0,0 @@
-/*
- * Decompiled with CFR 0.152.
- */
-package mk.com.snt.kc.warehouse.persistence;
-
-public interface Displayable {
-    public String getDisplay();
-}
--- warehouse-1.0-SNAPSHOT/WEB-INF/classes/mk/com/snt/kc/warehouse/persistence/Persistable.java
+++
@@ -1,8 +0,0 @@
-/*
- * Decompiled with CFR 0.152.
- */
-package mk.com.snt.kc.warehouse.persistence;
-
-public interface Persistable {
-    public Long getId();
-}
